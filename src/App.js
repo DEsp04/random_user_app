2a7c978773f118ec1b1c185a481b7f91580b5a18
david espinal <despinal0425@gmail.com>
2021-01-28 10:04:21 -0500

    api call was successful

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import fetchUser from "./services/rando_user";
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  function refreshUser() { 
+    console.log("refresh")
+  }
+
+  
+  useEffect(() => { 
+    const search = async () => {
+      const data = await fetchUser();
+
+      setCurrentUser(data);
+    }
+
+    search();
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Random User App</h1>
       </header>
+      
+      <button onClick={() => { 
+       refreshUser()
+      }}>Refresh</button>
+
+
+      <div>{JSON.stringify(currentUser)}</div> 
     </div>
   );
 }
