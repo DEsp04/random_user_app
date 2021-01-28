5c11f5b5332d0203b0595534a97dae0a2c74af39
david espinal <despinal0425@gmail.com>
2021-01-28 13:56:22 -0500

    done with project

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -9,11 +9,13 @@ function UserInfo(props) {
 
     return (
       <div>
+        <img src={props.userInfo.results[0].picture.large} />
         <p>Name: {props.userInfo.results[0].name.first} {props.userInfo.results[0].name.last}</p>
-
         <p>E-mail: {props.userInfo.results[0].email}</p>
-
         <p>Street: {props.userInfo.results[0].location.street.name}</p>
+        <p>City: {props.userInfo.results[0].location.city}</p>
+        <p>State: {props.userInfo.results[0].location.state}</p>
+        <p>User Name: {props.userInfo.results[0].login.username}</p>
       </div>
 
     )
